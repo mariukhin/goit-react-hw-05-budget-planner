f1d111a6e8c8d0b5d14c829de9187eb905b6627a
mariukhin <maryuhin.z@gmail.com>
2019-06-21 10:02:21 +0300

    Updates

---
+++ precache-manifest.2a365ac8489288c4eb844a2a1810b1d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "549cb78f4abd9d607011",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime~main.0cda3cff.js"
+  },
+  {
+    "revision": "25514ed0cac10ef25273",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.da9429af.chunk.js"
+  },
+  {
+    "revision": "d75057683afe6b7faa8a",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.4331a97d.chunk.js"
+  },
+  {
+    "revision": "25514ed0cac10ef25273",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.67040729.chunk.css"
+  },
+  {
+    "revision": "fa782c30768e33e5e1281155abaadec9",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  }
+];
